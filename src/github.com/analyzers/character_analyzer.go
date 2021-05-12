2a9be2f7e80172d822722eb058ea69b1c43307d4
Jorge Alonso Gastelum Gonzalez <jorgeg15@live.com.mx>
2021-05-11 21:53:56 -0600

    [CHALLENGE-2] Add the code to solve challenge 2

--- src/github.com/analyzers/character_analyzer.go
+++ src/github.com/analyzers/character_analyzer.go
@@ -1,13 +1,52 @@
 package analyzers
 
+import (
+	"rick-and-morty-challenge/src/github.com/models"
+	"strconv"
+)
+
 type CharactersAnalyzer struct {
-	analyzer RickAnalyzer
-	url      string
+	analyzer      RickAnalyzer
+	url           string
+	CharactersMap map[string]string
 }
 
 func (character *CharactersAnalyzer) Init() {
 	character.url = "https://rickandmortyapi.com/api/character"
 	character.analyzer = RickAnalyzer{URL: character.url}
+}
+
+func (character CharactersAnalyzer) GetOriginNames(url string, page string, channel chan models.Character) {
+	response := character.analyzer.MakeRequest(url + "?page=" + page)
+	results := response["results"].([]interface{})
+	for i := range results {
+		characterModel := models.Character{}
+		result := results[i].(map[string]interface{})
+		id := strconv.Itoa(int(result["id"].(float64)))
+		characterModel.URL = url + "/" + id
+		characterModel.Origin = result["origin"].(map[string]interface{})["name"].(string)
+		channel <- characterModel
+	}
+}
+
+func (character *CharactersAnalyzer) GetAll() {
+	baseURL := "https://rickandmortyapi.com/api/character"
+	character.CharactersMap = make(map[string]string)
+	info, _ := character.analyzer.GetInformation(baseURL)
+	numPages := int(info["pages"].(float64))
+	totalCharacters := int(info["count"].(float64))
+
+	channel := make(chan models.Character)
+
+	for i := 1; i <= numPages; i++ {
+		page := strconv.Itoa(i)
+		go character.GetOriginNames(baseURL, page, channel)
+	}
+
+	for i := 1; i < totalCharacters; i++ {
+		characterModel := <-channel
+		character.CharactersMap[characterModel.URL] = characterModel.Origin
+	}
 }
 
 func (character CharactersAnalyzer) GetAllNames(channel chan string) {
---
+++ src/github.com/models/character.go
@@ -0,0 +1,6 @@
+package models
+
+type Character struct {
+	URL    string
+	Origin string
+}
